d9a3effa33c3c65afbc2c42c1da7649bb4a8e815
cesckrodriguez <104809109+cesckrodriguez@users.noreply.github.com>
2022-07-03 20:20:46 -0500

    validacion

--- file.js
+++ file.js
@@ -2,31 +2,52 @@
 
 let clientes = [
     usuario1={
-        nombre: "Francisco ",
-        apellido: "Rodriguez ",
-        usuario: "frodriguez ",
-        contraseña: "1111 ",
-        saldo: "500 "
+        nombre: "Francisco",
+        apellido: "Rodriguez",
+        usuario: "frodriguez",
+        contraseña: "1111",
+        saldo: "500"
     },
     usuario2={
-        nombre: "Luis ",
-        apellido: "Perez ",
-        usuario: "lperez ",
-        contraseña: "2222 ",
-        saldo: "1000 "
+        nombre: "Luis",
+        apellido: "Perez",
+        usuario: "lperez",
+        contraseña: "2222",
+        saldo: "1000"
     },
     usuario3={
-        nombre: "Esperanza ",
-        apellido: "Campos ",
-        usuario: "ecampos ",
-        contraseña: "3333 ",
-        saldo: "500 "
+        nombre: "Esperanza",
+        apellido: "Campos",
+        usuario: "ecampos",
+        contraseña: "3333",
+        saldo: "500"
     },
     usuario4={
-        nombre: "Rosa ",
-        apellido: "Salcedo ",
-        usuario: "rsalcedo ",
-        contraseña: "4444 ",
+        nombre: "Rosa",
+        apellido: "Salcedo",
+        usuario: "rsalcedo",
+        contraseña: "4444",
         saldo: "500"
     }
 ];
+
+
+
+
+let registro = document.querySelector("#formulario-ingresar");
+
+registro.addEventListener("click",ingresarUsuarios);
+
+function ingresarUsuarios(){
+    let usuario = document.querySelector("#formulario-usuario");
+    let contraseña = document.querySelector("#formulario-contraseña");
+
+    for(let i=0; i<clientes.length; i++){
+        if(usuario === clientes[i].usuario && contraseña === clientes[i].contraseña){
+            alert("El usuario ha sido reconocido exitosamente.");
+            break;
+        }else{
+            alert("El usuario o la contraseña es incorrecto.");
+        }
+    }
+}
