203e42f830624e6b307dec3f3cee8d5d66d5fac4
cesckrodriguez <104809109+cesckrodriguez@users.noreply.github.com>
2022-07-03 19:04:21 -0500

    agregando estilos y elaboracion del vector de usuarios

---
+++ file.js
@@ -0,0 +1,32 @@
+//Vector contenedor de usuarios
+
+let clientes = [
+    usuario1={
+        nombre: "Francisco ",
+        apellido: "Rodriguez ",
+        usuario: "frodriguez ",
+        contraseña: "1111 ",
+        saldo: "500 "
+    },
+    usuario2={
+        nombre: "Luis ",
+        apellido: "Perez ",
+        usuario: "lperez ",
+        contraseña: "2222 ",
+        saldo: "1000 "
+    },
+    usuario3={
+        nombre: "Esperanza ",
+        apellido: "Campos ",
+        usuario: "ecampos ",
+        contraseña: "3333 ",
+        saldo: "500 "
+    },
+    usuario4={
+        nombre: "Rosa ",
+        apellido: "Salcedo ",
+        usuario: "rsalcedo ",
+        contraseña: "4444 ",
+        saldo: "500"
+    }
+];
